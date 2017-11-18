a75a4da88763ad20aa7d6f708396a2d21d6e5968
jiragangel <asbaticos@up.edu.ph>
2017-11-18 21:13:46 +0800

    Added back-end

---
+++ back-end/models/Movie.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const Questions = new Schema({
+  Easy: { type: Array, default: '' },
+  Medium: { type: Array, default: '' },
+  Difficult: { Array: Number }
+});
+
+mongoose.model('Questions', MovieSchema);
