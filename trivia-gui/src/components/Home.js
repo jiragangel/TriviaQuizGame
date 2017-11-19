bb89546d6a8ecc8afcae822eb8298c388c1d261a
jiragangel <asbaticos@up.edu.ph>
2017-11-20 01:03:32 +0800

    Fixed Errors of HEAD

--- trivia-gui/src/components/Home.js
+++ trivia-gui/src/components/Home.js
@@ -8,15 +8,9 @@ class Home extends Component{
 		           <h1>Trivia Quiz</h1>
 		            <div class="quizArea">
 		                <div class="quizHeader">
-<<<<<<< HEAD
 		                    <a class="button" href="/categories">Start Game</a>
 		                    <a class="button" href="/HighScore">High Scores</a>
 		                    <a class="button" href="/Questions">Questions</a>
-=======
-		                    <a class="button" href="/">Start Game</a>
-		                    <a class="button" href="/">High Scores</a>
-		                    <a class="button" href="/">Questions</a>
->>>>>>> f69c0fb27fe6af2c7301262c3a5435bad430a547
 		                </div>
 		            </div>
 		        </body>
