b08364a41d06da0eed617dbe226ecca977035c44
a-dreya <atcuevas2@up.edu.ph>
2017-11-20 09:29:01 +0800

    .

---
+++ trivia-gui/src/components/ManageQ.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+class ManageQ extends Component{
+	render(){
+		return(
+			<div className="App">
+		        <body className="container">
+		           <h1>Manage Questions</h1>
+		            <div class="quizArea">
+		                <div class="quizHeader">
+		                    <a class="button" href="/">Start Game</a>
+		                    <a class="button" href="/">High Scores</a>
+		                    <a class="button" href="/">Questions</a>
+		                </div>
+		            </div>
+		        </body>
+		    </div>
+		);
+	}
+}
+
+export default Home;
