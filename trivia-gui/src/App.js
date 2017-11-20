ec1059fc7c9f92b0e21afa747ca017e2b982aab3
a-dreya <atcuevas2@up.edu.ph>
2017-11-20 09:30:04 +0800

    .

--- trivia-gui/src/App.js
+++ trivia-gui/src/App.js
@@ -4,6 +4,8 @@ import Home from './components/Home';
 import Quiz from './components/Quiz';
 import Highscore from './components/Highscore';
 import Grade from './components/Grade';
+import Categories from './components/Categories'
+
 import  { BrowserRouter as Router, Route} from 'react-router-dom'
 
 class App extends Component {
@@ -13,9 +15,10 @@ class App extends Component {
         <Router>
           <div>
             <Route exact={true} path="/" component={Home} />
-            <Route exact={true} path="/quiz" component={Quiz} />
+            <Route exact={true} path="/categories" component={Categories} />
+            <Route exact={true} path="/quiz/:one/:two/:three/:noOfQuestions" component = {Quiz} />
             <Route exact={true} path="/highscore" component={Highscore} />
-            <Route exact={true} path="/grade" component={Grade} />
+            <Route exact={true} path="/quiz/grade" component={Grade} />
           </div>
         </Router>
       </div>
