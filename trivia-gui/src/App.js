881a6efb0f75a87094142512d123976bd38315c0
a-dreya <atcuevas2@up.edu.ph>
2017-11-27 07:43:30 +0800

    Edit Questions

--- trivia-gui/src/App.js
+++ trivia-gui/src/App.js
@@ -13,6 +13,7 @@ import ViewCategories from './components/ViewCategories'
 import AddCategories from './components/AddCategories'
 import DeleteCategories from './components/DeleteCategories'
 import DeleteQuestions from './components/DeleteQuestions'
+import EditQuestions from './components/EditQuestions'
 
 import  { BrowserRouter as Router, Route} from 'react-router-dom'
 
@@ -35,6 +36,7 @@ class App extends Component {
             <Route exact={true} path="/addCategories" component={AddCategories} />
             <Route exact={true} path="/DeleteCategories" component={DeleteCategories} />
             <Route exact={true} path="/DeleteQuestions" component={DeleteQuestions} />
+            <Route exact={true} path="/EditQuestions" component={EditQuestions} />
           </div>
         </Router>
       </div>
--- trivia-gui/src/components/Manage.js
+++ trivia-gui/src/components/Manage.js
@@ -9,13 +9,16 @@ class Manage extends Component{
 			            <h3>Questions</h3>
 		                <a className="manage-btn" href="/manage/view">View</a>
 		                <a className="manage-btn" href="/manage/add">Add</a>
-		                <a className="manage-btn" href="/DeleteQuestions">Edit/Delete</a>
+		                <a className="manage-btn" href="/EditQuestions">Edit</a>
+		                <a className="manage-btn" href="/DeleteQuestions">Delete</a>
+
 		            </div>
 		            <div className="manage">
 			            <h3>Categories</h3>
 		                <a className="manage-btn" href="/viewCategories">View</a>
 		                <a className="manage-btn" href="/addCategories">Add</a>
-		                <a className="manage-btn" href="/DeleteCategories">Edit/Delete</a>
+		                <a className="manage-btn" href="/EditQuestions">Edit</a>
+		                <a className="manage-btn" href="/DeleteCategories">Delete</a>
 		            </div>
 		            <a className="home-btn" href="/">Back</a>
 		        </div>
